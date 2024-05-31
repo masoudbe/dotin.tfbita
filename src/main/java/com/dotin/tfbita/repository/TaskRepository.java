86dfefcb799151e2a3eae7015680e8ed72b7a6e6
masoud <masoudbe@gmail.com>
2024-05-31 15:03:29 +0330

    import test jdl

---
+++ src/main/java/com/dotin/tfbita/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.dotin.tfbita.repository;
+
+import com.dotin.tfbita.domain.Task;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Task entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {}
---
+++ src/test/java/com/dotin/tfbita/domain/RegionTest.java
@@ -0,0 +1,39 @@
+package com.dotin.tfbita.domain;
+
+import static com.dotin.tfbita.domain.CountryTestSamples.*;
+import static com.dotin.tfbita.domain.RegionTestSamples.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.dotin.tfbita.web.rest.TestUtil;
+import org.junit.jupiter.api.Test;
+
+class RegionTest {
+
+    @Test
+    void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Region.class);
+        Region region1 = getRegionSample1();
+        Region region2 = new Region();
+        assertThat(region1).isNotEqualTo(region2);
+
+        region2.setId(region1.getId());
+        assertThat(region1).isEqualTo(region2);
+
+        region2 = getRegionSample2();
+        assertThat(region1).isNotEqualTo(region2);
+    }
+
+    @Test
+    void countryTest() throws Exception {
+        Region region = getRegionRandomSampleGenerator();
+        Country countryBack = getCountryRandomSampleGenerator();
+
+        region.setCountry(countryBack);
+        assertThat(region.getCountry()).isEqualTo(countryBack);
+        assertThat(countryBack.getRegion()).isEqualTo(region);
+
+        region.country(null);
+        assertThat(region.getCountry()).isNull();
+        assertThat(countryBack.getRegion()).isNull();
+    }
+}
